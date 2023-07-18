198c1e84c9b067ab53e7732be6698abc9db305a7
Henok <henokwehibe@gmail.com>
2023-07-18 23:11:24 +0100

    removed bootstrap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(),astroImageTools]
 });
