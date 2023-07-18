a758065faf3aa5aca0729f10085af8041b7c8baf
Henok <henokwehibe@gmail.com>
2023-07-18 22:46:01 +0100

    added swiper

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+console.log("Swiper Astro")
+
+
+var swiper = new Swiper('.swiper-container', {
+    speed: 500,
+    effect: 'fade',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
