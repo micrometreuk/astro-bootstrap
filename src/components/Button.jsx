43db4a1b10e3900a84ef60aff89645c5cfdcded0
Henok <henokwehibe@gmail.com>
2023-07-18 23:36:16 +0100

    added added

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = ({ children }) => {
-  return (
-    <button className="mx-auto text-center mb-5 btn btn-primary"  >{ children }</button>
-  )
-}
-
-export default Button;
