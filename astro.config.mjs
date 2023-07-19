ccc7cfbbee7f06feed3cf46c3d957846ef41590c
Henok <henokwehibe@gmail.com>
2023-07-19 09:49:01 +0100

    adjusting html

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import { astroImageTools } from "astro-imagetools";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(),astroImageTools]
+  integrations: [react(), tailwind()]
 });
--- public/js/main.js
+++ public/js/main.js
@@ -2,11 +2,14 @@ console.log("Swiper Astro")
 
 
 var swiper = new Swiper('.swiper-container', {
-    speed: 500,
-    effect: 'fade',
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  speed: 500,
+  effect: 'fade',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 5000,
+  },
 
-  });
+});
